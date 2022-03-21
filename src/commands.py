5e2ddc618a3295c37cb8aeacb2a9662d7f46dfae
suwa <43295199+suwuako@users.noreply.github.com>
2022-03-22 03:27:31 +0800

    Add files via upload

--- src/commands.py
+++ src/commands.py
@@ -1,3 +1,5 @@
+import json
+
 from datetime import datetime
 from discord.ext import commands
 
@@ -7,8 +9,12 @@ class commands(commands.Cog):
         self.start_time = datetime.now()
         print("loaded commands")
 
-    @commands.command()
+    @commands.command(aliases=["active", "online"])
     async def ping(self, message):
+        """
+        generic ping comand to check bot's latency && to check if bot is still respondive
+        """
+
         then = datetime.now()
 
         message = await message.send("<:sip:778194172762128414> Pong!")
@@ -21,6 +27,30 @@ class commands(commands.Cog):
         await message.edit(content=f"<:sip:778194172762128414> Pong!\n"
                                    f"[latency: {latency}] (ms) \n"
                                    f"[edit latency: {edit_latency}] (ms)")
+
+    @commands.command(aliases=["up", "time"])
+    async def uptime(self, message):
+        """
+        generic uptime command to check bot's uptime
+        """
+
+        uptime = (datetime.now() - self.start_time)
+
+        output = f"`[{datetime.now().strftime('%H:%M:%S | %d/%m/%Y')}]` I have been online for `{uptime}`, since `{self.start_time} (GMT+8)`"
+        await message.reply(output)
+
+    @commands.command(aliases=["count"])
+    async def hopes(self, message):
+        """
+        shows the total amount of times `hope` has been called since 2022-03-22 02:56:48.040721 (GMT+8)
+        """
+
+        hopes = json.load(open(r"json/hopes.json"))[0]
+
+        await message.reply(f"Since `2022-03-22 02:56:48.040721 (GMT+8)`, {hopes} hopes have been posted.")
+
+
+
 
 def setup(bot):
     bot.add_cog(commands(bot))
--- src/listening.py
+++ src/listening.py
@@ -8,15 +8,31 @@ class listening(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         urls = open(r"json/urls.json")
+        hope_count = open(r"json/hopes.json")
 
         self.urls = json.load(urls)
+        self.hopes = json.load(hope_count)[0]
 
+        print("loaded listening module...")
     
     @commands.Cog.listener()
     async def on_message(self, message):
+        if message.author.bot:
+            return
+
         content = message.content.lower()
+
+        if "!hopes" in content:
+            return
+
         if "hope" in content or "hoping" in content:
             await message.reply(random.choice(self.urls))
+            print(f"Hope requested from {message.author}")
+            self.hopes += 1
+
+            with open(r"json/hopes.json", 'w') as file:
+                json.dump([self.hopes], file)
+
 
 
 def setup(bot):
